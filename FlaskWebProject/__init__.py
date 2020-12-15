e0238c392b4d2d8857fdbea4c1d6184f2082b410
Loc Nguyen <vinhloc30796@gmail.com>
2020-12-15 18:11:10 +0700

    Add logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,11 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.INFO)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+app.logger.addHandler(stream_handler)
+# Start the app
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
